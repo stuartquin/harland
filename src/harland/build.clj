0e18bb0c1c8b7cb0ca2d579806626c8b556f769e
Stuart Quin <stuart.quin@gmail.com>
2014-10-25 16:55:13 +0100

    Push message to build queue

--- src/harland/build.clj
+++ src/harland/build.clj
@@ -1,5 +1,17 @@
 (ns harland.build
-  (require [me.raynes.conch :as sh]))
+  (require [me.raynes.conch :as sh]
+           [taoensso.carmine :as car :refer (wcar)]))
+
+; TODO move this to conf file or something
+(def master-redis "redis://localhost:6379")
+(def server-conn {:pool {} :spec {:uri master-redis}})
+(defmacro wcar* [& body] `(car/wcar server-conn ~@body))
+
+(defn schedule-build
+  "Schedule a build"
+  [project-name]
+  (wcar* 
+    (car/lpush "build-queue" project-name)))
 
 (defn build
   "Runs a build using conch"
